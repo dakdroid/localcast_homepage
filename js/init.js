52eb7577d79654239b7e4bd4a19578987c77dfc7
Stefan Pledl <stefanpledl@googlemail.com>
2014-12-28 14:51:00 +0100

    initial

---
+++ js/init.js
@@ -0,0 +1,7 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({menuWidth: 240, activationWidth: 70});
+ $('.modal-trigger').leanModal();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
